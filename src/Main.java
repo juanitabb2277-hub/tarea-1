cd5e1922a0d1d821ca7fa14deeb65a394dec0d59
juanita <juanitabb2277@gmail.com>
2025-09-04 13:14:08 -0500

    aleatorio

--- src/Main.java
+++ src/Main.java
@@ -1,15 +1,27 @@
-//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
-// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
+import java.util.*;
 public class Main {
     public static void main(String[] args) {
-        //TIP Press <shortcut actionId="ShowIntentionActions"/> with your caret at the highlighted text
-        // to see how IntelliJ IDEA suggests fixing it.
-        System.out.printf("Hello and welcome!");
-
-        for (int i = 1; i <= 5; i++) {
-            //TIP Press <shortcut actionId="Debug"/> to start debugging your code. We have set one <icon src="AllIcons.Debugger.Db_set_breakpoint"/> breakpoint
-            // for you, but you can always add more by pressing <shortcut actionId="ToggleLineBreakpoint"/>.
-            System.out.println("i = " + i);
+        Random ale =new Random();
+        int n3=0,i,j = 0;
+        for (i=1; i<=100; i++){
+            n3=ale.nextInt((100-1)+1)+1;
+            System.out.println(n3);
+            j+=n3;
         }
+        System.out.println(j);
+//generar 100 numeros aleatorios entre 1-200 y cuanto es la suma de los pares y cuanto de los impares
+        Random al =new Random();
+        int n4,m,pares = 0,impares=0;
+        for (m=1; m<=100; m++){
+            n4=al.nextInt((100-1)+1)+1;
+            System.out.println(n3);
+            if (n4%2==0){
+               pares+=n4;
+            } else if (n4%2!=0) {
+                impares=n4+impares;
+            }
+        }
+        System.out.println("La suma de los pares es: " +pares);
+        System.out.println("La suma de los impares es: " +impares);
     }
 }
